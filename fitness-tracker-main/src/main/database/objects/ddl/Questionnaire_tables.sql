032333b5de9e4cbdf25f267a14aadb6787c68cf0
jneil <justin.neil@thomsonreuters.com>
2018-12-05 20:17:25 -0500

    Finished Questionnaire_tables.sql - need to comment all schema...

--- fitness-tracker-main/src/main/database/objects/ddl/Questionnaire_tables.sql
+++ fitness-tracker-main/src/main/database/objects/ddl/Questionnaire_tables.sql
@@ -24,7 +24,7 @@ CREATE TABLE QS_Question
 	QuestionID 		BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1),
 	QuestionnaireID	BIGINT NOT NULL,
 	QuestionValue	VARCHAR(2000) NOT NULL,
-	OrderBy			INT,
+	OrderBy			INT NOT NULL,
 	RequiredFlag	CHAR(1) NOT NULL,
 	CreatedDate		TIMESTAMP NOT NULL,
 	CreatedUserID	BIGINT NOT NULL,
@@ -51,10 +51,18 @@ CREATE TABLE QS_Answer
 
 CREATE TABLE QS_QuestionnaireTemplate
 (
-
+	QuestionnaireTemplateID BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1),
+	TemplateName VARCHAR(250) NOT NULL,
+	QuestionnaireName VARCHAR(250),
+	CreatedDate		TIMESTAMP NOT NULL,
+	CreatedUserID	BIGINT NOT NULL,
+	ModifiedDate	TIMESTAMP NOT NULL,
+	ModifiedUserID	BIGINT NOT NULL,
+	ActiveFlag		CHAR(1) NOT NULL,
+	CONSTRAINT PK_QS_QuestionnaireTemplate PRIMARY KEY (QuestionnaireTemplateID)
 );
 
-???CREATE TABLE QS_QuestionnaireTemplateMap
+CREATE TABLE QS_QuestionnaireTemplateMap
 (
 	QuestionnaireTemplateMapID	BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1),
 	QuestionnaireTemplateID	BIGINT NOT NULL,
@@ -65,17 +73,38 @@ CREATE TABLE QS_QuestionnaireTemplate
 	ModifiedDate	TIMESTAMP NOT NULL,
 	ModifiedUserID	BIGINT NOT NULL,
 	ActiveFlag		CHAR(1) NOT NULL,
-	CONSTRAINT PK_QS_QuestionMap PRIMARY KEY (QuestionMapID)
+	CONSTRAINT PK_QS_QuestionTemplateMapID PRIMARY KEY (QuestionnaireTemplateMapID)
 );
 
 CREATE TABLE QS_QuestionTemplate
 (
-
+	QuestionTemplateID 		BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1),
+	QuestionValue	VARCHAR(2000) NOT NULL,
+	RequiredFlag	CHAR(1) NOT NULL,
+	QuestionTypeCode BIGINT NOT NULL,
+	CreatedDate		TIMESTAMP NOT NULL,
+	CreatedUserID	BIGINT NOT NULL,
+	ModifiedDate	TIMESTAMP NOT NULL,
+	ModifiedUserID	BIGINT NOT NULL,
+	ActiveFlag		CHAR(1) NOT NULL,
+	CONSTRAINT PK_QS_QuestionTemplateID PRIMARY KEY(QuestionTemplateID)
 );
 
+-- This needs work - it might not be multi choice. 
+-- What about - select all that apply, rank these
 CREATE TABLE QS_MultiChoiceAnswer
 (
-
+	MultiChoiceAnswerID		BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1),
+	AnswerStringValue		VARCHAR(2000),
+	AnswerNumericValue		BIGINT,
+	AnswerBooleanValue		CHAR(1),
+	CreatedDate		TIMESTAMP NOT NULL,
+	CreatedUserID	BIGINT NOT NULL,
+	ModifiedDate	TIMESTAMP NOT NULL,
+	ModifiedUserID	BIGINT NOT NULL,
+	ActiveFlag		CHAR(1) NOT NULL,
+	CONSTRAINT PK_QS_MultiChoiceAnswer PRIMARY KEY(MultiChoiceAnswerID)
+	
 );
 
 
